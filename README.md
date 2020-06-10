046273636a7a5537e82af9a55538049344fa6270
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-10 03:24:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# W14D2-MoreRelationships
+ more relationship models with a seeding dataloader
---
+++ src/test/java/com/example/codeclan/MoreRelationships/MoreRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.MoreRelationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
